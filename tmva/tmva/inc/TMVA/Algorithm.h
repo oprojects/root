4ce27138f646879f132dab8537fd384d047a169a
Omar Zapata <Omar.Zapata@cern.ch>
2016-08-24 11:59:08 +0200

    TMVA: working in documentation

--- tmva/tmva/inc/TMVA/Algorithm.h
+++ tmva/tmva/inc/TMVA/Algorithm.h
@@ -75,38 +75,106 @@ namespace TMVA {
           
       public:
           /**
-           Constructor for the initialization of Algorithms,
+           Default destructor
            */
           ~Algorithm();
           
-          virtual void BookMethod( TString methodName, TString methodTitle, TString theOption = "");
-          virtual void BookMethod( Types::EMVA theMethod,  TString methodTitle, TString theOption = "");
+          /**
+            Method to book the machine learning method to perform the algorithm.
+            \param methodname String with the name of the mva method
+            \param methodtitle String with the method title.
+            \param options String with the options for the method.
+          */
+          virtual void BookMethod( TString methodname, TString methodtitle, TString options = "");
+          /**
+            Method to book the machine learning method to perform the algorithm.
+            \param method enum TMVA::Types::EMVA with the type of the mva method
+            \param methodtitle String with the method title.
+            \param options String with the options for the method.
+          */
+          virtual void BookMethod( Types::EMVA method,  TString methodtitle, TString options = "");
 
-          //file related methods
+          /**
+            Method to see if a file is available to save results
+            \return Boolean with the status.
+          */
           Bool_t  IsSilentFile();
+          /**
+            Method to get the pointer to TFile object.
+            \return pointer to TFile object.
+          */
           TFile* GetFile();
+          /**
+            Method to set the pointer to TFile object,
+            with a writable file.
+            \param file pointer to TFile object.
+          */
           void   SetFile(TFile *file);
           
-          //dataloader related methods
+          /**
+            Method to get the pointer to TMVA::DataLoader object.
+            \return  pointer to TMVA::DataLoader object.
+          */
           DataLoader *GetDataLoader();
+          
+          /**
+            Method to set the pointer to TMVA::DataLoader object.
+            \param file pointer to TFile object.
+          */
           void SetDataLoader(DataLoader *dalaloader);
 
-          //model persistence
-          Bool_t IsModelPersistence();//is you Algorithm saved in files?
+          /**
+            Method to see if the algorithm model is saved in xml or serialized files.
+            \return Boolean with the status.
+          */
+          Bool_t IsModelPersistence();
+          
+          /**
+            Method enable model persistence, then algorithms model is saved in xml or serialized files.
+            \param status Boolean with the status.
+          */
           void SetModelPersistence(Bool_t status=kTRUE);
           
-          //verbose mode related methods
-          Bool_t IsVerbose(){return fVerbose;}
-          void SetVerbose(Bool_t status){fVerbose=status;}
+          /**
+            Method to see if the algorithm should print extra information.
+            \return Boolean with the status.
+          */
+          Bool_t IsVerbose();
           
+          /**
+            Method enable print extra information in the algorithms.
+            \param status Boolean with the status.
+          */
+          void SetVerbose(Bool_t status);
+          
+          /**
+            Virtual method to be implmented with your algorithm.
+          */
           virtual void Evaluate() = 0;
           
       protected:
+          /**
+            Method get the Booked method in a option map object.
+            \return TMVA::OptionMap with the information of the Booked method
+          */
           OptionMap &GetMethod();
           
-          //dataloader related utility methods
+          /**
+            Utility method to get TMVA::DataInputHandler reference from the DataLoader.
+            \return TMVA::DataInputHandler reference.
+          */
           DataInputHandler&        GetDataLoaderDataInput() { return *fDataLoader->fDataInputHandler; }
+          
+          /**
+            Utility method to get TMVA::DataSetInfo reference from the DataLoader.
+            \return TMVA::DataSetInfo reference.
+          */
           DataSetInfo&             GetDataLoaderDataSetInfo(){return fDataLoader->DefaultDataSetInfo();}
+          
+          /**
+            Utility method to get TMVA::DataSetManager pointer from the DataLoader.
+            \return TMVA::DataSetManager pointer.
+          */
           DataSetManager*          GetDataLoaderDataSetManager(){return fDataLoader->fDataSetManager;}
           ClassDef(Algorithm,0);
           
--- tmva/tmva/src/Algorithm.cxx
+++ tmva/tmva/src/Algorithm.cxx
@@ -15,36 +15,53 @@
 
 using namespace TMVA;
 
+//______________________________________________________________________________
 Algorithm::Algorithm(const TString &name,DataLoader *dalaloader,TFile *file,const TString options):Configurable(options,name),fDataLoader(dalaloader),fFile(file),fVerbose(kFALSE)
 {}
 
+//______________________________________________________________________________
 Algorithm::~Algorithm()
 {}
 
+//______________________________________________________________________________
 Bool_t  Algorithm::IsSilentFile(){return fFile==nullptr;}
 
+//______________________________________________________________________________
 TFile* Algorithm::GetFile(){return fFile.get();}
 // TFile* Algorithm::GetFile(){return fFile==nullptr?0:fFile.get();}
 
+//______________________________________________________________________________
 void   Algorithm::SetFile(TFile *file){fFile=std::shared_ptr<TFile>(file);}
 
+//______________________________________________________________________________
+Bool_t Algorithm::IsVerbose(){return fVerbose;}
+
+//______________________________________________________________________________
+void Algorithm::SetVerbose(Bool_t status){fVerbose=status;}
+
+//______________________________________________________________________________
 OptionMap &Algorithm::GetMethod(){     return fMethod;}
 
+//______________________________________________________________________________
 DataLoader *Algorithm::GetDataLoader(){    return fDataLoader.get();}
 
+//______________________________________________________________________________
 void Algorithm::SetDataLoader(DataLoader *dalaloader){
         fDataLoader=std::shared_ptr<DataLoader>(dalaloader) ;
 }
 
+//______________________________________________________________________________
 Bool_t TMVA::Algorithm::IsModelPersistence(){return fModelPersistence; }
 
+//______________________________________________________________________________
 void TMVA::Algorithm::SetModelPersistence(Bool_t status){fModelPersistence=status;}
 
-
+//______________________________________________________________________________
 void TMVA::Algorithm::BookMethod(Types::EMVA method, TString methodTitle, TString options){
     return BookMethod(Types::Instance().GetMethodName( method ),methodTitle,options);
 }
 
+//______________________________________________________________________________
 void TMVA::Algorithm::BookMethod(TString methodName, TString methodTitle, TString options){
     fMethod["MethodName"]    = methodName;
     fMethod["MethodTitle"]   = methodTitle;
